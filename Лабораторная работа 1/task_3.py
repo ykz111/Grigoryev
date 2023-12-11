41747c6ff29f9c6ae9de07abaee99a39da496bbf
ykz111 <153546656+ykz111@users.noreply.github.com>
2023-12-12 00:07:49 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,5 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+n = int(len(list_players)/2)
+# TODO Разделите участников на две команды
+print(list_players[0:n])
+print(list_players[n:2*n])
